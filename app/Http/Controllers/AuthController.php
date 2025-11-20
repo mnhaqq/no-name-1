66c00e78c448ef10ea01273aee28604aee66d3be
Haqq Bassit <munagahmnh@gmail.com>
2025-11-20 11:54:12 +0000

    Implement login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -25,7 +26,25 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
-        return 'login';
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, 
+        $user->password)) {
+            return [
+                'message' => 'Provided credentials are incorrect'
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function logout(Request $request) {
